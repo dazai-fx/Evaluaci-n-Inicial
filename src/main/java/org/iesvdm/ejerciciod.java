82a9e6cc6937fd86dc9dec41d216250707f82981
José Antonio <83926859+dazai-fx@users.noreply.github.com>
2025-09-18 14:39:47 +0200

    Add files via upload

--- src/main/java/org/iesvdm/ejerciciod.java
+++ src/main/java/org/iesvdm/ejerciciod.java
@@ -1,7 +1,31 @@
 package org.iesvdm;
 
+import java.util.Scanner;
+
 public class ejerciciod {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un número:");
+
+        int num = sc.nextInt();
+
+        System.out.println("Introduce la posición del digito que quieras: ");
+
+        int posicion = sc.nextInt();
+
+        String numString = Integer.toString(num);
+
+        if(posicion > numString.length() || posicion <=0){
+            System.out.println("No existe el dígito en esa posición");
+        }else{
+            char digito = numString.charAt(posicion - 1);
+            int digitoInt = Character.getNumericValue(digito);
+            System.out.println("El dígito en la posición " + posicion + " es: " + digitoInt);
+        }
+        sc.close();
+
+
     }
 }
---
+++ src/main/java/org/iesvdm/ejercicioe.java
@@ -0,0 +1,16 @@
+package org.iesvdm;
+
+import java.util.Scanner;
+
+public class ejercicioe {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce la altura de la pirámide maya: ");
+
+        int altura = sc.nextInt();
+
+
+    }
+}
